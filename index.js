e757e7441a49ab3b5e456216457c819d8431fa7d
student online <eliel.badon@gmail.com>
2018-07-31 18:25:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,31 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+var itemprices = []
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  } else if (cart.length === 1) {
+    itemprices.push(`${cart[0].itemName} at $${cart[0].itemPrice}`)
+  }
+  else if (cart.length === 2) {
+  itemprices.push(`${cart[0].itemName} at $${cart[0].itemPrice}`)
+  itemprices.push(` and ${cart[1].itemName} at $${cart[1].itemPrice}`)
+  itemprices.join()
+     } 
+  else if (cart.length === 3) {
+    for (var i = 0; i < cart.length; i++) {
+      if (i === (cart.length - 1)) {
+        itemprices.push(` and ${cart[i].itemName} at $${cart[i].itemPrice}`)
+      } else if (i === 0) {
+      itemprices.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+      } else {
+      itemprices.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`);
+      }
+      itemprices.join(`,`)
+    }
+  }
+  return `In your cart, you have ${itemprices}.`
+
 }
 
 function total() {
